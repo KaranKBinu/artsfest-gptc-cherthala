302dc7d813f4bb1d9c615d4f4cd5e44886545a9b
KaranKBinu <your-email@example.com>
2026-02-16 14:27:07 +0530

    feat: Add Google site verification.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,9 @@ export async function generateMetadata(): Promise<Metadata> {
       description,
       images: [icon],
     },
+    verification: {
+      google: '2a2b2787f7d55980',
+    },
   }
 }
 
